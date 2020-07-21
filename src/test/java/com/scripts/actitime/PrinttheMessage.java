b41bd5c47eb988b87840f289c4e00b8f78ad04ab
SANJIT VK <SANJIT VK@DESKTOP-6SF9HCC.Home>
2020-07-21 16:22:00 +0100

    Added new testclass

---
+++ src/test/java/com/scripts/actitime/PrinttheMessage.java
@@ -0,0 +1,13 @@
+package com.scripts.actitime;
+
+import org.testng.annotations.Test;
+
+public class PrinttheMessage 
+{
+	@Test
+	public void print()
+	{
+		System.out.println("i am printing");
+	}
+
+}
